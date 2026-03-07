d190d2e9f2215f24f662f4c79ec93866e8d7d4d2
Qinghua24 <zhangqh24@mails.tsinghua.edu.cn>
2026-03-07 11:02:09 +0800

    initial commit

---
+++ src/cellseg/_reader.py
@@ -0,0 +1,85 @@
+"""
+This module is an example of a barebones numpy reader plugin for napari.
+
+It implements the Reader specification, but your plugin may choose to
+implement multiple readers or even other plugin contributions. see:
+https://napari.org/stable/plugins/building_a_plugin/guides.html#readers
+"""
+
+import numpy as np
+
+
+def napari_get_reader(path):
+    """A basic implementation of a Reader contribution.
+
+    Parameters
+    ----------
+    path : str or list of str
+        Path to file, or list of paths.
+
+    Returns
+    -------
+    function or None
+        If the path is a recognized format, return a function that accepts the
+        same path or list of paths, and returns a list of layer data tuples.
+    """
+    if isinstance(path, list):
+        # reader plugins may be handed single path, or a list of paths.
+        # if it is a list, it is assumed to be an image stack...
+        # so we are only going to look at the first file.
+        path = path[0]
+
+    # the get_reader function should make as many checks as possible
+    # (without loading the full file) to determine if it can read
+    # the path. Here, we check the dtype of the array by loading
+    # it with memmap, so that we don't actually load the full array into memory.
+    # We pretend that this reader can only read integer arrays.
+    try:
+        arr = np.load(path, mmap_mode='r')
+        if arr.dtype != np.int_:
+            return None
+    # napari_get_reader should never raise an exception, because napari
+    # raises its own specific errors depending on what plugins are
+    # available for the given path, so we catch
+    # the OSError that np.load might raise if the file is malformed
+    except OSError:
+        return None
+
+    # otherwise we return the *function* that can read ``path``.
+    return reader_function
+
+
+def reader_function(path):
+    """Take a path or list of paths and return a list of LayerData tuples.
+
+    Readers are expected to return data as a list of tuples, where each tuple
+    is (data, [add_kwargs, [layer_type]]), "add_kwargs" and "layer_type" are
+    both optional.
+
+    Parameters
+    ----------
+    path : str or list of str
+        Path to file, or list of paths.
+
+    Returns
+    -------
+    layer_data : list of tuples
+        A list of LayerData tuples where each tuple in the list contains
+        (data, metadata, layer_type), where data is a numpy array, metadata is
+        a dict of keyword arguments for the corresponding viewer.add_* method
+        in napari, and layer_type is a lower-case string naming the type of
+        layer. Both "meta", and "layer_type" are optional. napari will
+        default to layer_type=="image" if not provided
+    """
+    # handle both a string and a list of strings
+    paths = [path] if isinstance(path, str) else path
+    # load all files into array
+    arrays = [np.load(_path) for _path in paths]
+    # stack arrays into single array
+    data = np.squeeze(np.stack(arrays))
+
+    # optional kwargs for the corresponding viewer.add_* method
+    add_kwargs = {}
+
+    layer_type = 'image'  # optional, default is "image"
+    return [(data, add_kwargs, layer_type)]
